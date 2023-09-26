25b4f712cc231d904df532e2cccfb2ab71049caa
Csontos <csontostibor48@gmail.com>
2023-09-26 09:09:14 +0200

    ulm diagram értelmezése,kezdő értékek,Like eljárás

--- BejegyzesProject/Bejegyzes.cs
+++ BejegyzesProject/Bejegyzes.cs
@@ -8,7 +8,24 @@ namespace BejegyzesProject
 {
     internal class Bejegyzes
     {
-    
+        public string Szerzo { get; }
+        public string Tartalom { get; set; }
+        public int Likeok { get; private set; }
+        public DateTime Letrejott { get; }
+        public DateTime Szerkesztve { get; private set; }
+
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            Szerzo = szerzo;
+            Tartalom = tartalom;
+            Likeok = 0;
+            Letrejott = Szerkesztve = DateTime.Now;
+        }
+
+        public void Like()
+        {
+            Likeok++;
+        }
     }
 
 }
