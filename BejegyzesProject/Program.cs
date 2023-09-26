2c6ff39872730b3a35622e5071e9f6c94f0cde78
Csontos <csontostibor48@gmail.com>
2023-09-26 10:12:37 +0200

    bjegyzes szam letrehozasa bekert parameterekkel +error message+teszt

--- BejegyzesProject/Program.cs
+++ BejegyzesProject/Program.cs
@@ -10,7 +10,7 @@ namespace BejegyzesProject
     {
         static void Main(string[] args)
         {
-            List<Bejegyzes> bLista = new List<Bejegyzes>();
+           /* List<Bejegyzes> bLista = new List<Bejegyzes>();
 
             bLista.Add(new Bejegyzes("sz1", "első tartalma."));
             bLista.Add(new Bejegyzes("sz2", "második tartalma."));
@@ -18,8 +18,39 @@ namespace BejegyzesProject
             foreach (Bejegyzes bejegyzes in bLista)
             {
                 Console.WriteLine(bejegyzes.ToString());
+            }*/
+
+            List<Bejegyzes> bLista = new List<Bejegyzes>();
+
+            Console.Write("Bejegyzések Száma: ");
+            if (int.TryParse(Console.ReadLine(), out int darabszam) && darabszam > 0)
+            {
+                for (int i = 0; i < darabszam; i++)
+                {
+                    Console.Write($"Add meg a {i + 1}.  szerzőjét: ");
+                    string szerzo = Console.ReadLine();
+
+                    Console.Write($"Add meg a {i + 1}. tartalmát: ");
+                    string tartalom = Console.ReadLine();
+
+                    bLista.Add(new Bejegyzes(szerzo, tartalom));
+                }
+
+                Console.WriteLine("\nelkeszult:");
+                foreach (Bejegyzes bejegyzes in bLista)
+                {
+                    Console.WriteLine(bejegyzes.ToString());
+                }
             }
+            else
+            {
+                Console.WriteLine("pozití,egész számot írj");
+            }
+
             Console.ReadKey();
+
+
         }
+
     }
 }
